e47e496e4120181e168d50edf2507b5020223752
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-06 12:13:19 +0000

    task two completed, brute force

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,14 +1,21 @@
 use std::fs;
-use std::error::Error;
 
 fn main() {
     let mut file = fs::read_to_string("day_one_input.txt").expect("AAAAAA");
-    let inputs  = file.split_whitespace();
+    
+    run_day_one(&file);
+    run_day_two(&file);
+    
+}
+
+fn run_day_one(file_info: &str) {
+
+    let inputs  = file_info.split_whitespace();
     let mut safe = Safe {
         dial:50
     };
     let mut passcode = 0;
-   for instruction in inputs {
+    for instruction in inputs {
         //println!("sections: {instruction:?}");
 
         let (digit, lr):(String, String) = instruction.chars().partition(|c| c.is_digit(10));
@@ -19,9 +26,31 @@ fn main() {
         
     }
     println!("passcode: {passcode}");
+
 }
 
+fn run_day_two(file_info: &str) {
 
+    let inputs = file_info.split_whitespace();
+    let mut safe = Safe {
+        dial:50
+    };
+    let mut passcode = 0;
+
+    for instruction in inputs {
+        let (digit, lr):(String, String) = instruction.chars().partition(|c| c.is_digit(10));
+        let turns = digit.parse::<usize>().unwrap();
+        let amount = String::from("1");
+        for _ in 0..turns {
+          
+            safe.turn(&amount, &lr);
+            if safe.dial == 0 {
+                passcode += 1;
+            }
+        }        
+    }
+     println!("passcode tast 2: {passcode}");
+}
 
 struct Safe {
     dial: u32,
