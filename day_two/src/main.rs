dee497f0cc71fdd64e42eb2f55034f2c683e0e70
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-07 14:24:19 +0000

    refactored and broke

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -3,35 +3,64 @@ use fancy_regex::Regex;
 use std::thread;
 use std::sync::LazyLock;
 use std::num::ParseIntError;
+use std::error::Error;
+
 
 use std::time::Instant;
 
-static MY_REGEX: LazyLock<Regex> =
+static DAY_ONE_REGEX: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r"^(\d+)\1$").unwrap());
+static DAY_TWO_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^(\d+)\1+$").unwrap());
 
 fn main() {
 
     let file_string = fs::read_to_string("day_two_input.txt").expect("no file");
     let string_split = FullId::new_ids(&file_string).unwrap();
-
     let number_of_threads = std::thread::available_parallelism().unwrap().get();
     let target_chunks = number_of_threads;//4-8 heuristic for  - CORE dependant heuristic is approx num of cores
     let chunk_size = string_split.range.len() / target_chunks;
 
     let start = Instant::now();
     
+    run(string_split, chunk_size, DaySwitch::One);
+    
+    let duration = start.elapsed();
+    println!("time: {:?}", duration.as_millis());
+
+}
+
+#[derive(Debug, Clone, Copy)]
+enum DaySwitch {
+    One,
+    Two,
+}
+
+fn run(full_id: FullId, chunk_size: usize, day_switch: DaySwitch) 
+{
+
     let result:u64 = thread::scope(|scope| {
-        let chunked = string_split.range.chunks(chunk_size);
+    let chunked = full_id.range.chunks(chunk_size);
         
-        let mut handles = vec![];
+    let mut handles = vec![];
         for ids in chunked {
             
             handles.push(scope.spawn(move || {
                 let mut count = 0;
                 for id in ids {
-                    if let Ok(true) = id.is_valid() {
+                    match day_switch {
 
-                        count += id.id.parse::<u64>().unwrap();
+                       DaySwitch::One => { 
+                            if let Ok(true) = id.is_valid() {
+
+                                count += id.id.parse::<u64>().unwrap();
+                            }
+                        },
+                        DaySwitch::Two => {
+                            if let Ok(true) = id.is_valid_task_2() {
+                                count += id.id.parse::<u64>().unwrap();
+                            }
+                        }
                     }
                 }
                 count
@@ -42,11 +71,8 @@ fn main() {
             .map(|handle| handle.join().unwrap())
             .sum()
     });
-    
          
     println!("{result}");
-    let duration = start.elapsed();
-    println!("time: {:?}", duration.as_millis());
 }
 
 #[derive(Debug)]
@@ -76,7 +102,7 @@ impl FullId{
         })
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ID {
     id: String,
 }
@@ -89,6 +115,10 @@ impl ID {
     }
 
     fn is_valid(&self) -> Result<bool, fancy_regex::Error> {
-        MY_REGEX.is_match(&self.id)
+        DAY_ONE_REGEX.is_match(&self.id)
+    }
+
+    fn is_valid_task_2(&self) -> Result<bool, fancy_regex::Error> {
+        DAY_TWO_REGEX.is_match(&self.id)
     }
 }
