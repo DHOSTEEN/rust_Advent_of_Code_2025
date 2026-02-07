e01737a3f874b6cbc8f41d0dcdf749fe9c49cfd1
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-07 11:32:42 +0000

    working, unclean code, no error handling

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -1,64 +1,113 @@
 use std::fs;
 use fancy_regex::Regex;
+use std::thread;
+use std::sync::LazyLock;
+
+static MY_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^(\d+)\1$").unwrap());
+
+
 
 fn main() {
 
-    let file_string = fs::read_to_string("day_two_test.txt").expect("no file");
+    let file_string = fs::read_to_string("day_two_input.txt").expect("no file");
     let string_split = Full_ID::new_IDs(&file_string);
-    //let split = string_split.split(",");
-    //println!("splits: {:?}", string_split);
-    let mut count = 0;
-    for i in string_split {
 
+    let number_of_threads = std::thread::available_parallelism().unwrap().get();
+    println!("MINE THREADS: {number_of_threads:?}");
+    let target_chunks = number_of_threads;//4-8 heuristic
+    let chunk_size = string_split.range.len() / target_chunks;
 
-            if i.first.is_valid() {
-                count += 1;
-            }
-            if i.last.is_valid() {
-                count += 1;
-            }
+    println!("range of list: {:?}\nchunk size: {:?}", string_split.range.len(), chunk_size);
+
+    
+    let result:u64 = thread::scope(|scope| {
+        let chunked = string_split.range.chunks(chunk_size);
         
-    }
-    println!("{count}");
+        let mut handles = vec![];
+        for ids in chunked {
+            
+            handles.push(scope.spawn(move || {
+                let mut count = 0;
+                for id in ids {
+                    if id.is_valid() {
+                        println!("{}", id.id);
+                        count += id.id.parse::<u64>().unwrap();
+                    }
+                    
+                }
+                count
+            }));
+        }
+
+        handles
+            .into_iter()
+            .map(|handle| handle.join().unwrap())
+            .sum()
+    });
+    
+         
+    println!("{result}");
 }
+    //
+ 
+
 #[derive(Debug)]
-struct Full_ID<'a> {
-    first: ID<'a>,
-    last: ID<'a>,
+struct Full_ID {
+    range: Vec<ID>
 }
 
-impl <'a>Full_ID<'a> {
-    pub fn new_IDs (all_ids: &'a str) -> Vec<Self> {
-        let mut all_ID:Vec<Self> = vec![];
+impl Full_ID{
+    pub fn new_IDs<'a> (all_ids: &'a str) -> Self {
+        let mut all_ID:Vec<ID> = vec![];
         for raws in all_ids.split(",") {
             let raws = raws.trim();
             let mut raw_split = raws.split("-");
-            let id = Full_ID {
-                first: ID::new(raw_split.next()),
-                last: ID::new(raw_split.next()),
-            };
-            all_ID.push(id);
+            let start = raw_split.next().unwrap().parse::<usize>().unwrap();
+            let end = raw_split.next().unwrap().parse::<usize>().unwrap() + 1;
+
+            for i in start..end {
+
+                all_ID.push(ID::new(i.to_string()));
+            }
+            
         }
-        all_ID
+        Full_ID{
+            range: all_ID
+        }
+       
     }
 
 }
 #[derive(Debug)]
-struct ID<'a> {
-    id: &'a str,
+struct ID {
+    id: String,
 }
 
-impl<'a> ID<'a> {
-    pub fn new (raw: Option<&'a str>) -> Self {
+impl ID {
+    pub fn new (id: String) -> Self {
         ID {
-            id: raw.unwrap(),
+            id,
         }
     }
 
     fn is_valid(&self) -> bool {
         
-        let rs = Regex::new(r"^(\d+)\1").unwrap();
-        println!("{:?} is {}", self.id, Regex::is_match(&rs, self.id).unwrap());
-        Regex::is_match(&rs, self.id).unwrap()
+
+        //println!("{:?} is {}", &self.id.to_string(), Regex::is_match(&rs, &self.id.to_string()).unwrap());
+        //Regex::is_match(&rs, &self.id.to_string()).unwrap()
+        /*if let Some(stuff) = MY_REGEX.find(&self.id.to_string()).unwrap(){
+            let first = &stuff;
+            println!("first val: {:?}", first);
+            //return self.id.to_string().len() == self.length;
+            return true;
+        }
+        
+        false*/
+        match MY_REGEX.find(&self.id).unwrap() {
+            Some(_) => return true,
+            None => return false,
+        }
+
     }
 }
