7a66a4f0c9d4d64ffab2716f0e6260d16fb4f464
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-09 21:21:02 +0000

    mostly refactored

---
+++ day_two/src/error.rs
@@ -0,0 +1,19 @@
+use thiserror::Error;
+use std::num::ParseIntError;
+
+#[derive(Debug, Error)]
+pub enum Day2Error {
+    #[error("failed to parse ID AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa")]
+    ParseID(#[from] ParseIntError),
+
+    #[error("fancy regex failed")]
+    FancyRegex(#[from] fancy_regex::Error),
+
+    #[error("no start point from id string")]
+    NoStart,
+    #[error("no end point from id string")]
+    NoEnd,
+
+    #[error("thread paniced")]
+    ThreadPanic,
+}
--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -2,9 +2,10 @@ use std::fs;
 use fancy_regex::Regex;
 use std::thread;
 use std::sync::LazyLock;
-use std::num::ParseIntError;
 
 use std::time::Instant;
+mod error;
+use error::Day2Error;
 
 static DAY_ONE_REGEX: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r"^(\d+)\1$").unwrap());
@@ -19,8 +20,10 @@ fn main() {
     let target_chunks = number_of_threads;//4-8 heuristic for  - CORE dependant heuristic is approx num of cores
     let chunk_size = string_split.range.len() / target_chunks;
 
-    let start = Instant::now();   
-    run(&string_split, chunk_size, DaySwitch::One);    
+    let start = Instant::now();
+
+    run(&string_split, chunk_size, DaySwitch::One);
+ 
     let duration = start.elapsed();
     println!("time task one: {:?}", duration.as_millis());
 
@@ -37,43 +40,54 @@ enum DaySwitch {
     Two,
 }
 
-fn run(full_id: &FullId, chunk_size: usize, day_switch: DaySwitch) 
+fn run(full_id: &FullId, chunk_size: usize, day_switch: DaySwitch)
 {
 
-    let result:u64 = thread::scope(|scope| {
+    let result:Result<u64, Day2Error> = thread::scope(|scope| {
     let chunked = full_id.range.chunks(chunk_size);
         
     let mut handles = vec![];
         for ids in chunked {
             
-            handles.push(scope.spawn(move || {
+            handles.push(scope.spawn(move || -> Result<_, Day2Error> {
                 let mut count = 0;
                 for id in ids {
                     match day_switch {
 
                        DaySwitch::One => { 
-                            if let Ok(true) = id.is_valid() {
 
-                                count += id.id.parse::<u64>().unwrap();
+                        let valid = id.is_valid()?;
+                            if valid {
+                                count += id.id.parse::<u64>()?;
                             }
                         },
                         DaySwitch::Two => {
-                            if let Ok(true) = id.is_valid_task_2() {
-                                count += id.id.parse::<u64>().unwrap();
+                            let valid = id.is_valid_task_2()?;
+                            if valid {
+                                count += id.id.parse::<u64>()?;
                             }
                         }
                     }
                 }
-                count
+                Ok(count)
             }));
         }
-        handles
-            .into_iter()
-            .map(|handle| handle.join().unwrap())
-            .sum()
+
+        let mut result:u64 = 0;
+
+        for handle in handles {
+            let val = handle
+                        .join()
+                        .map_err(|_| Day2Error::ThreadPanic)??;
+            result += val; 
+        }
+        Ok(result)
+
     });
+
+    println!("{result:?}")
          
-    println!("{result}");
+    
 }
 
 #[derive(Debug)]
@@ -81,28 +95,34 @@ struct FullId {
     range: Vec<ID>
 }
 
-impl FullId{
-    pub fn new_ids<'a> (all_ids: &'a str) -> Result<Self, ParseIntError> {
-
+impl FullId {
+    pub fn new_ids(all_ids: &str) -> Result<Self, Day2Error> {
         let range = all_ids
-        .split(',')
-        .try_fold(Vec::new(),|mut range, raw| {
-            let mut parts = raw.trim().split('-');
+            .split(',')
+            .try_fold(Vec::new(), |mut range, raw| -> Result<_, Day2Error> {
+                let mut parts = raw.trim().split('-');
 
-            let start = parts.next().unwrap().parse::<usize>()?;
-            let end   = parts.next().unwrap().parse::<usize>()?;
+                let start = parts
+                    .next()
+                    .ok_or(Day2Error::NoStart)?
+                    .parse::<usize>()?;
 
-            for num in start..=end {
-                range.push(ID::new(num.to_string()));
-            }
-            Ok(range)
-        })?;
-        
-        Ok(FullId{
-            range,
-        })
+                let end = parts
+                    .next()
+                    .ok_or(Day2Error::NoEnd)?
+                    .parse::<usize>()?;
+
+                for num in start..=end {
+                    range.push(ID::new(num.to_string()));
+                }
+
+                Ok(range)
+            })?;
+
+        Ok(Self { range })
     }
 }
+
 #[derive(Debug, Clone)]
 struct ID {
     id: String,
@@ -115,11 +135,13 @@ impl ID {
         }
     }
 
-    fn is_valid(&self) -> Result<bool, fancy_regex::Error> {
-        DAY_ONE_REGEX.is_match(&self.id)
+    fn is_valid(&self) -> Result<bool, Day2Error> {
+        let result = DAY_ONE_REGEX.is_match(&self.id)?;
+        Ok(result)
     }
 
-    fn is_valid_task_2(&self) -> Result<bool, fancy_regex::Error> {
-        DAY_TWO_REGEX.is_match(&self.id)
+    fn is_valid_task_2(&self) -> Result<bool, Day2Error> {
+        let result = DAY_TWO_REGEX.is_match(&self.id)?;
+        Ok(result)
     }
 }
