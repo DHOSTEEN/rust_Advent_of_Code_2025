d66c08fda633f009badf3194996c3e821df4046d
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-19 19:19:13 +0000

    task two works, no major reworks required. refactoring WIP

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -4,21 +4,41 @@ fn main() {
     let lines = file_reader::read_lines("day4_input.txt").unwrap();
     let mut raws = vec![];
     for line in lines.map_while(Result::ok) {
-        println!("{}", line);
+        //println!("{}", line);
         raws.push(line);
     }
-    let grid = Grid::new(raws);
+    let mut grid = Grid::new(raws);
+    let mut final_count = 0_u32;
+    let mut first_count = 0_u32;
 
-    let mut smart_rows = build_smart(&grid);
-    count_valid(&smart_rows);
-    println!("\nAAAAAAAAAAAA\n");
-    for x in &grid {
-        println!("{x:?}");
+    loop {
+        let mut smart_rows = build_smart(&grid);
+        let removals_and_count = count_valid(&smart_rows);
+        let (removals, count) = removals_and_count;
+        if count == 0 {break;}
+        final_count += count;
+        if first_count == 0 {
+            first_count = count;
+        }
+
+        //println!("\nAAAAAAAAAAAA\n");
+        for (col, row) in removals.iter() {
+      
+           // println!("col, row: {},{}", col, row);
+            let mut stuff = grid.rows.get_mut(*col).unwrap();
+            stuff.replace_range(*row..=*row, "x");
+        
+        //x.replace_range(1..2, "7");
+        }
+        //for (i,x) in grid.rows.iter_mut().enumerate() {
+        //    println!("{:?}",x );
+        //}
     }
+    println!("first count: {}\nfinal count: {}", first_count, final_count);
 
 }
 
-fn count_valid<'a>(rows: &[Row<'a>]) {
+fn count_valid<'a>(rows: &[Row<'a>]) -> (Vec<(usize, usize)>, u32) {
     let mut safe_count:u32 = 0;
     let mut col = 0;
     let mut row = 0;
@@ -178,10 +198,11 @@ fn count_valid<'a>(rows: &[Row<'a>]) {
         col += 1;
 
     }
-    println!("row count: {safe_count}");
-    for rem in to_remove {
+    //println!("row count: {safe_count}");
+    /*for rem in to_remove {
         println!("{:?}", rem);
-    }
+    }*/
+    (to_remove, safe_count)
     //
     // 0@1@2@3@4@5.6x7.8@9@
 }
