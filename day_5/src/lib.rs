483eacf55ec53875e5984ce226c6813e91f32fb1
Programming Acolyte <programmingacolyte@gmail.com>
2026-02-23 10:52:39 +0000

    cleaned code and added doc for merge_ranges

--- day_5/src/lib.rs
+++ day_5/src/lib.rs
@@ -13,15 +13,50 @@ pub fn parse_range(raw_range: &str) -> Result<[u64;2], Day5Error> {
     Ok(arr)
 }
 
-///this is SUPER fast
+///O(n log n) Sorts ranges by internal array's first element, merges ranges, and returns a new vector with merged ranges
+///
+///the returning vector contains:
+///
+///A) the unaltered elements of incoming ranges if no merge happens
+///
+///B) merged ranges done via the upper bound.
+///
+///This is done by first sorting the incoming list by the first element (START) to ensure no possible range overlaps are missed
+///through a single pass merge.
+///We then create a vector to store all results of the merge and loop through all incoming ranges.
+///
+///A merge occurs when the incoming range is both within the 'last' element of the merged list and has a greater upper bound
+///with: 'if let Some(last) = merged.last() && range[START] < last[END]'
+///
+///ie: last = [10, 14] range = [12, 18]
+///
+///12 < 14
+///
+///Then we take the greater upper bound of both and update last:
+///
+///last = [10, 18]
+///
+///this allows all ranges to be merged in one pass of the incoming ranges, 0(n), so the greatest cost of this function is the sort
+///# Example
+///```
+///use day_5::merge_ranges;
+///let test_ranges = vec![[16,20], [3,5], [10,14], [12,18]];
+///let expected = vec![[3,5], [10, 20]];
+///let result = merge_ranges(test_ranges);
+///assert_eq!(result, expected);
+///```
 pub fn merge_ranges(mut ranges: Vec<[u64; 2]>) -> Vec<[u64; 2]> {
+    let test_ranges = vec![[16,20], [3,5], [10,14], [12,18]];
     ranges.sort_unstable_by_key(|range| range[START]);// O(n log n)
+    //no need for START == START comparisons to care about orginal order so unstable is fine
     let mut merged:Vec<[u64;2]> = Vec::new();
 
     for range in ranges {//O(n)
+
         if let Some(last) = merged.last_mut() 
-            && range[0] <= last[END] + 1 {//inclusive
-                last[1] = last[END].max(range[END]);
+            && range[START] < last[END] {
+                
+                last[END] = last[END].max(range[END]);
                 continue;
         }
         
@@ -29,43 +64,4 @@ pub fn merge_ranges(mut ranges: Vec<[u64; 2]>) -> Vec<[u64; 2]> {
     }
 
     merged
-}
-
-///kept for comparison to O(n log n) function megre_ranges
-///O(n^2)
-#[allow(dead_code)]
-pub fn combine_id_ranges(left :&[u64;2], right: &[u64;2]) -> Option<[u64;2]> {
-    
-    if left[START] < right[START] && left[END] > right[END] {
-        ////println!("left: {},{}\nright: {},{}", left[START], left[END], right[START], right[END] );
-        return Some(*left);
-    }
-    if right[START] < left[START] && right[END] > left[END] {
-        return Some(*right);
-    }
-
-    //left bounds
-
-    if left[START] < right[START]  && left[END] >= right[START]{
-        //left is lower bounded
-        //println!("LEFT HAS LOWER FIRST:\nleft: {},{}\nright: {},{}", left[START], left[END], right[START], right[END]);
-        return Some([left[START], right[END]]);
-    }
-    if left[END] > right[END] {
-        //left is higher bounded
-        //println!("LEFT HAS HIGHER SECOND:\nleft: {},{}\nright: {},{}", left[START], left[END], right[START], right[END]);
-        return Some([right[START], left[END]]);
-    }
-
-    //right bounds
-
-    if right[START] < left[START] {
-        return Some([right[START], left[END]]);
-    }
-    if right[END] > left[END] && left[END] >= right[START] {
-        //println!("RIGHT HAS HIGHER SECOND:\nleft: {},{}\nright: {},{}", left[START], left[END], right[START], right[END]);
-        return Some([left[START], right[END]]);
-    }
-
-    None
 }
--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -18,7 +18,6 @@ fn main() -> Result<(), Day5Error> {
         } else if switch { 
             let new_ids = parse_range(&line)?;
             fresh_ids.push(new_ids);
-            //println!("{fresh_ids:?}");
         } else {
             available_ids.push(line.parse::<u64>()?);
         }
@@ -26,24 +25,21 @@ fn main() -> Result<(), Day5Error> {
     let mut count = 0;
     fresh_ids.sort_by_key(|arr| arr[0]);
     for available in available_ids {
-        //println!("{available}");
         if fresh_ids.iter().any(|arr| arr[0] < available && arr[1] >= available){
-            //println!("{arr:?}");
             count += 1;
         }
     }
-    println!("\nfinal result {count}");
+    println!("\ntask one result: {count}");
 
 
     let fresh_ids = merge_ranges(fresh_ids);
-    
-    println!("INTO THE COUNT!");
+
     let final_count:u64 =
     fresh_ids.into_iter()
         .map(|[start, end]| start.abs_diff(end) +1)
         .sum();
 
-    println!("{final_count}");
+    println!("task two result: {final_count}");
     
     Ok(())
 }
