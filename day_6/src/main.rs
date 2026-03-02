7aa0e362933fbc5ae8764d0d765d6a83beaf4e42
Programming Acolyte <programmingacolyte@gmail.com>
2026-03-02 10:11:04 +0000

    dev for char matrix

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -5,55 +5,30 @@ use day_6::{Colum, OperationSymbol};
 mod task_one;
 mod task_two;
 fn main() -> Result<()> {
-    let file = file_reader::read_lines("day6_input.txt")?;
+    let file = file_reader::read_lines("day6_test.txt")?;
 
     let mut all_cols_task_one: Vec<Colum> = Vec::new();
-    let mut all_cols_task_two: Vec<Colum> = Vec::new();
-    let mut col_width: usize = 0;
+    let mut all_rows_task_two: Vec<Vec<char>> = Vec::new();
 
     for line in file.map_while(Result::ok) {
 
         println!("{line}");
 
         task_one::line_parseing(&line, &mut all_cols_task_one)?;
-        task_two::line_parseing(&line, &mut all_cols_task_two)?;
+
+        let mut row = Vec::new();
+        for ch in line.chars() {
+            row.push(ch);
+        }
+        all_rows_task_two.push(row);
     }
+    task_two::line_parseing(&all_rows_task_two)?;
 
     // for all in all_cols {
     //     println!("{:?}", all);
     // }
 
     let result: u64 = all_cols_task_one.iter().map(|col| col.caculate()).sum();
-    println!("{result:?}");
-
-    let cols:Vec<_> = all_cols_task_two.iter().map(|col| col.caculate_t2()).collect();
-    let mut symbol:OperationSymbol = OperationSymbol::Plus;
-    let mut partial_result:u64 = 0;
-    let mut result:u64 = 0;
-    for (num, maybe_symbol) in cols.into_iter().map_while(Result::ok) {
-        use OperationSymbol::*;
-        println!("Number incoming: {:?}", num);
-        if let Some(new_symbol) = maybe_symbol{        
-            result += partial_result;
-            println!("AFTER NEW COL: partial: {} full: {}", partial_result, result);
-            symbol = new_symbol;
-            partial_result = 0;
-        }
-        
-        match symbol {
-            Multiply => {if partial_result == 0 {
-                partial_result += 1;
-                }
-                if let Some(num) = num{
-                    partial_result *= num;
-                }
-            }
-            Plus => if let Some(num) = num{
-                    partial_result += num;
-                }
-        }
-        println!("partial: {} full: {}", partial_result, result);
-    } 
     println!("{result:?}");
 
     Ok(())
--- day_6/src/task_two.rs
+++ day_6/src/task_two.rs
@@ -1,50 +1,21 @@
-use crate::task_one;
 use day_6::Colum;
 use day_6::Day6Error;
 
-pub fn line_parseing(
-    line: &str,
-    all_cols: &mut Vec<Colum>
-) -> Result<(), Day6Error> {
- 
-  
-    for (i, bytes) in line.chars().enumerate() {
-       
-        let nth = all_cols.get_mut(i);
-        if let Some(col) = nth {
-    
-            let raw_num = bytes.to_string();
-            if raw_num == "*" || raw_num == "+" {
-                col.add_symbol(&raw_num)?;
-            } else{
-            //col.add_num(&raw_num)?;
-                col.add_string(&raw_num);
-            }
-        } else {
-            let mut col = Colum::default();
-            let raw_num = bytes.to_string();
-            //col.add_num(&raw_num)?;
-            col.add_string(&raw_num);
-            all_cols.push(col);
+pub fn line_parseing<'a>(
+    rows: &[Vec<char>]
+) -> Result<Vec<Vec<char>>, Day6Error> {
+    let row_count = rows.len();
+    let col_count = rows[0].len();
+
+    let mut cols = vec![Vec::with_capacity(row_count); col_count];
+
+    for row in rows {
+        for (j, elem) in row.into_iter().enumerate() {
+            cols[j].push(*elem);
         }
     }
-
-    Ok(())
-}
-
-pub fn calculate_col_width(line: &str) -> usize {
-    let mut max: u32 = 0;
-    let mut count = 0;
-    //println!("{line}");
-    for ch in line.chars() {
-        if ch == ' ' {
-            if max < count {
-                max = count;
-            }
-            count = 0;
-        } else {
-            count += 1;
-        }
+    for x in cols.iter() {
+        println!("{:?}", x);
     }
-    max as usize
+    Ok(cols)
 }
